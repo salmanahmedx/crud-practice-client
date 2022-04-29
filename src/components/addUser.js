1427e87d47857b4125f4f9b5bae26ee25a93cdb2
Salman Ahmed <salmanahmedx@hotmail.com>
2022-04-29 12:05:10 +0600

    first commit

---
+++ src/components/addUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const addUser = () => {
+    return (
+        <div>
+            <h1>Add user plz</h1>
+        </div>
+    );
+};
+
+export default addUser;
---
+++ src/components/currUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const currUser = () => {
+    return (
+        <div>
+            <h1>i am curr user</h1>
+        </div>
+    );
+};
+
+export default currUser; <h1>i am curr user</h1>
